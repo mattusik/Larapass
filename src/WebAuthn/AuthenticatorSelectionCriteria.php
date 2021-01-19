e127ba1ce5c1d931980e02d8567072f08207a74b
Matus Rohal <matus@rohal.biz>
2021-01-19 13:02:02 +0100

    Fixed declarations in AuthenticatorSelectionCriteria.php according to the parent class

--- src/WebAuthn/AuthenticatorSelectionCriteria.php
+++ src/WebAuthn/AuthenticatorSelectionCriteria.php
@@ -11,20 +11,21 @@ class AuthenticatorSelectionCriteria extends WebAuthnAuthenticatorSelectionCrite
     /**
      * Sets the Resident Key variable.
      *
-     * @param  string  $type
+     * @param string|null $residentKey
+     * @return AuthenticatorSelectionCriteria
      */
-    public function setResidentKey(string $type)
+    public function setResidentKey(?string $residentKey): self
     {
-        if (! in_array($type, [self::USER_VERIFICATION_REQUIREMENT_REQUIRED,
+        if (! in_array($residentKey, [self::USER_VERIFICATION_REQUIREMENT_REQUIRED,
             self::USER_VERIFICATION_REQUIREMENT_PREFERRED,
             self::USER_VERIFICATION_REQUIREMENT_DISCOURAGED], false)) {
-            throw new \RuntimeException("The {$type} as Resident Key option is unsupported.");
+            throw new \RuntimeException("The {$residentKey} as Resident Key option is unsupported.");
         }
 
-        $this->residentKey = $type;
+        $this->residentKey = $residentKey;
     }
 
-    public function getResidentKey()
+    public function getResidentKey(): ?string
     {
         return $this->residentKey;
     }
